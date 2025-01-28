f95318a4cd2104c9e596a71a79ac6bd60595a1d6
anas-alnabahin <anasnb2002nb@gmail.com>
2025-01-28 14:08:22 +0200

    moving routes to a separate folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
 import { useEffect } from "react";
 
 import { ThemeProvider } from "styled-components";
-import { Route, Routes } from "react-router-dom";
 
-import { Layout } from "./routes/layout/layout.component";
-import TraineeManagement from "./routes/trainee-management/trainee-management.component";
-import EquipmentManagement from "./routes/equipment-management/equipment-management.component";
-import SalesManagement from "./routes/sales-management/sales-management.component";
-import ProgramsManagement from "./routes/programs-management/programs-management.component";
-import StaffManagement from "./routes/staff-management/staff-management.component";
-import ReportsAndAnalytics from "./routes/reports-and-analytics/reports-and-analytics.component";
-import Home from "./routes/home/home.component";
-import SignIn from "./routes/auth/sign-in.component";
-import EnterEmail from "./routes/auth/enter-email.component";
-import VerifyCode from "./routes/auth/verify-code.component";
-import NewPassword from "./routes/auth/new-password.component";
-
+import Router from "./router/router";
 import { useAppDispatch } from "./app/hooks";
 import { setCredentials } from "./features/auth/auth.slice";
 import { getCredentials } from "./utils/auth/auth.utils";
@@ -39,24 +26,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <ResetStyles />
         <GlobalStyle />
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="auth" element={<SignIn />} />
-            <Route path="forget-password" element={<EnterEmail />} />
-            <Route path="verify-code" element={<VerifyCode />} />
-            <Route path="new-password" element={<NewPassword />} />
-            <Route index element={<Home />} />
-            <Route path="trainees" element={<TraineeManagement />} />
-            <Route path="equipment" element={<EquipmentManagement />} />
-            <Route path="sales" element={<SalesManagement />} />
-            <Route path="programs" element={<ProgramsManagement />} />
-            <Route path="staff" element={<StaffManagement />} />
-            <Route
-              path="reports-and-analytics"
-              element={<ReportsAndAnalytics />}
-            />
-          </Route>
-        </Routes>
+        <Router />
       </ThemeProvider>
     </>
   );
---
+++ src/router/router.tsx
@@ -0,0 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Layout } from "../routes/layout/layout.component";
+import SignIn from "../routes/auth/sign-in.component";
+import EnterEmail from "../routes/auth/enter-email.component";
+import VerifyCode from "../routes/auth/verify-code.component";
+import NewPassword from "../routes/auth/new-password.component";
+import Home from "../routes/home/home.component";
+import TraineeManagement from "../routes/trainee-management/trainee-management.component";
+import EquipmentManagement from "../routes/equipment-management/equipment-management.component";
+import SalesManagement from "../routes/sales-management/sales-management.component";
+import ProgramsManagement from "../routes/programs-management/programs-management.component";
+import StaffManagement from "../routes/staff-management/staff-management.component";
+import ReportsAndAnalytics from "../routes/reports-and-analytics/reports-and-analytics.component";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="auth" element={<SignIn />} />
+        <Route path="forget-password" element={<EnterEmail />} />
+        <Route path="verify-code" element={<VerifyCode />} />
+        <Route path="new-password" element={<NewPassword />} />
+        <Route index element={<Home />} />
+        <Route path="trainees" element={<TraineeManagement />} />
+        <Route path="equipment" element={<EquipmentManagement />} />
+        <Route path="sales" element={<SalesManagement />} />
+        <Route path="programs" element={<ProgramsManagement />} />
+        <Route path="staff" element={<StaffManagement />} />
+        <Route path="reports-and-analytics" element={<ReportsAndAnalytics />} />
+      </Route>
+    </Routes>
+  );
+};
+export default Router;
