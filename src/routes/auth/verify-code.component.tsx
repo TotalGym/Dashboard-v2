0f6471a8ddb93f023fc4b8f689b9281186da16b0
anas-alnabahin <anasnb2002nb@gmail.com>
2025-02-03 14:43:59 +0200

    redirect user from enter-email, verify-code, and new-password pages when he is logged in

--- src/routes/auth/verify-code.component.tsx
+++ src/routes/auth/verify-code.component.tsx
@@ -7,9 +7,19 @@ import {
   AuthContainer,
   AuthFormContainer,
 } from "./auth.styles";
+import { useAppSelector } from "../../app/hooks";
+import { selectToken } from "../../features/auth/auth.slice";
+import { Navigate, useLocation } from "react-router-dom";
 
 const VerifyCode = () => {
-  return (
+  const token = useAppSelector(selectToken);
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
+
+  return token ? (
+    <Navigate to={from} replace />
+  ) : (
     <AuthContainer>
       <AuthBackGroundContainer $bgImage={authBgImage} />
       <AuthFormContainer>
