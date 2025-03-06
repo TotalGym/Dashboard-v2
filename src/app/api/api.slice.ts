14a70a00c3bff4aa831e9b1c4107380b605c7e26
anas-alnabahin <anasnb2002nb@gmail.com>
2025-03-06 11:44:21 +0200

    enhancing api requests to handle failed requests and timeouts

--- src/app/api/api.slice.ts
+++ src/app/api/api.slice.ts
@@ -1,16 +1,22 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
-const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_BASE_URL,
-  prepareHeaders: (headers, { getState }) => {
-    const token = (getState() as RootState).auth.token;
-    if (token) {
-      headers.set("Authorization", `Bearer ${token}`);
-    }
-    return headers;
-  },
-});
+const baseQuery = retry(
+  fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_BASE_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+    timeout: 10000,
+  }),
+  {
+    maxRetries: 3,
+  }
+);
 
 export const apiSlice = createApi({
   reducerPath: "api",
